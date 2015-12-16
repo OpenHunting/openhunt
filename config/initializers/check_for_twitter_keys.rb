4d5e81c332b30373dfdc55bd1487891963ded861
Jacques Crocker <railsjedi@gmail.com>
2015-12-16 15:00:10 -0800

    ignore twitter keys check when compiling assets

--- config/initializers/check_for_twitter_keys.rb
+++ config/initializers/check_for_twitter_keys.rb
@@ -1,9 +1,14 @@
-# Warn user if theres no twitter_key or twitter_secret configured
-if Settings.twitter_key.blank? or Settings.twitter_secret.blank?
-  puts "MISSING: `Settings.twitter_key` and/or `Settings.twitter_secret`"
-  if Rails.env.production?
-    puts("SET IT WITH: ENV['Settings.twitter_key'] and ENV['Settings.twitter_secret']")
-  else
-    puts("ADD A FILE: config/settings.local.yml (see config/settings.local.yml.example)")
+is_precompile = ARGV.any?{|i| i.to_s.include?("assets:precompile")}
+
+unless is_precompile
+  puts "HELLO"
+  # Warn user if theres no twitter_key or twitter_secret configured
+  if Settings.twitter_key.blank? or Settings.twitter_secret.blank?
+    puts "MISSING: `Settings.twitter_key` and/or `Settings.twitter_secret`"
+    if Rails.env.production?
+      puts("SET IT WITH: ENV['Settings.twitter_key'] and ENV['Settings.twitter_secret']")
+    else
+      puts("ADD A FILE: config/settings.local.yml (see config/settings.local.yml.example)")
+    end
   end
 end
