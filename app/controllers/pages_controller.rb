a51ce2ebda9fd60013bdffe0977a52e2c329398c
Jacques Crocker <railsjedi@gmail.com>
2015-12-17 02:36:04 -0800

    fix site on iphone

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,8 @@ class PagesController < ApplicationController
   end
 
   def audit_log
-    @audit_logs = AuditLog.all.includes(:moderator).order(:created_at => :desc)
+    @audit_logs = AuditLog.all.includes(:moderator).order(:created_at => :desc).limit(100)
+    # TODO: paginate
   end
 
 end
