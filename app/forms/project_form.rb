3a75e5bed9bb6ded99964719cc25999ca4c2bf4f
Mark Hurwitz <mhurwitzus@gmail.com>
2015-12-19 07:46:23 -0600

    ProjectForm works for updating and existing project, checks if it should update the url and validates as appropriate.

--- app/forms/project_form.rb
+++ app/forms/project_form.rb
@@ -7,8 +7,9 @@ class ProjectForm
 
   attribute :url
   validates_presence_of :url
-  validates_url :url, :allow_blank => true
-  validate :check_unique_url
+  # validates_url :url, :allow_blank => true # do we need to ever allow a blank url?
+  attr_accessor :skip_url_validation
+  validate :check_unique_url, unless: :skip_url_validation
   def check_unique_url
     return if url.blank?
 
@@ -23,5 +24,30 @@ class ProjectForm
   attribute :description
   validates_presence_of :description
   validates_length_of :description, maximum: 80, allow_blank: true
+
+  def self.for_update(params, project)
+    params = params.with_indifferent_access
+    form = ProjectForm.new(skip_url_validation: should_skip_url_validation?(params[:url], project.url))
+    form.name = params[:name] || project.name
+    form.url = params[:url] || project.url
+    form.description = params[:description] || project.description
+    form
+  end
+
+  def self.should_skip_url_validation?(params_url, project_url)
+    return true if params_url_is_very_similar?(params_url, project_url)
+    return true if params_url.blank?
+    return false
+  end
+
+  def self.params_url_is_very_similar?(params_url, project_url)
+    existing_normalized_url = Project.normalize_url(project_url)
+    existing_normalized_url = Project.slice_www(existing_normalized_url)
+    params_normalized_url = Project.normalize_url(params_url)
+    params_normalized_url = Project.slice_www(params_normalized_url)
+
+    return true if existing_normalized_url == params_normalized_url
+    return false
+  end
 
 end
---
+++ spec/forms/project_form_spec.rb
@@ -0,0 +1,32 @@
+
+require 'rails_helper'
+
+RSpec.describe ProjectForm do
+  let(:submitter) { FactoryGirl.create(:user) }
+  let(:project) { submitter.projects.create(FactoryGirl.attributes_for(:project)) }
+  before :each do
+    DatabaseCleaner.clean
+  end
+
+  context "for update" do
+    it "validates with just name" do
+      params = { name: "banana" }
+      form = ProjectForm.for_update(params, project)
+      expect(form.valid?).to eql true
+      expect(form.name).to eql "banana"
+    end
+    it "validates with just description" do
+      params = { description: "banana" }
+      form = ProjectForm.for_update(params, project)
+      expect(form.valid?).to eql true
+      expect(form.description).to eql "banana"
+    end
+    it "for_update skips validating url if trivial change" do
+      params = { url: "http://asdf.com" }
+      form = ProjectForm.for_update(params, project)
+      expect(form.valid?).to eql true
+      expect(form.url).to eql "http://asdf.com"
+    end
+  end
+
+end
