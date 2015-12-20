3325c2fef137c38251510d9717d23230556b30bd
Mark Hurwitz <mhurwitzus@gmail.com>
2015-12-20 10:02:24 -0600

    fix weekend bucket bugs in Project.prev_bucket and Project.next_bucket

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,44 +1,21 @@
-# == Schema Information
-#
-# Table name: projects
-#
-#  id              :integer          not null, primary key
-#  name            :string           not null
-#  description     :string           not null
-#  url             :string           not null
-#  normalized_url  :string           not null
-#  bucket          :string           not null
-#  slug            :string           not null
-#  user_id         :integer          not null
-#  hidden          :boolean          default(FALSE)
-#  votes_count     :integer          default(0)
-#  feedbacks_count :integer          default(0)
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#
-# Indexes
-#
-#  index_projects_on_bucket  (bucket)
-#  index_projects_on_slug    (slug)
-#
-
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+  before :each do
+    DatabaseCleaner.clean
+    new_time = Time.local(2015, 12, 15, 12, 0, 0)
+    Timecop.freeze(new_time)
+  end
+  after do
+    Timecop.return
+  end
 
   context "single project" do
     let(:submitter) { FactoryGirl.create(:user) }
     let(:project) { submitter.projects.create(FactoryGirl.attributes_for(:project)) }
     let(:submitter2) { FactoryGirl.create(:user) }
     let(:project_without_www) { submitter.projects.create(FactoryGirl.attributes_for(:project, url: "https://asdf.com")) }
-    before :each do
-      DatabaseCleaner.clean
-      new_time = Time.local(2015, 12, 15, 12, 0, 0)
-      Timecop.freeze(new_time)
-    end
-    after do
-      Timecop.return
-    end
+
     it "detects duplicate new url only lacks www" do
       project # create the project first, has url 'www.asdf.com'
       result = Project.get_duplicate_by_url("http://asdf.com")
@@ -67,6 +44,9 @@ RSpec.describe Project, type: :model do
       #   5     / (         4            + 2 ) **   1.8   = 0.412346
       expect(project.score.round(6)).to eql 0.412346
     end
+  end
+
+  context "buckets" do
     it "calculates the right bucket" do
       expect(Project.bucket(Time.now)).to eql "20151215"
     end
@@ -76,45 +56,29 @@ RSpec.describe Project, type: :model do
       expect(Project.bucket(saturday)).to eql "20151213"
       expect(Project.bucket(sunday)).to eql "20151213"
     end
+    it "gets prev bucket" do
+      expect(Project.prev_bucket(Time.now)).to eql "20151214"
+    end
+    it "prev bucket is a Friday if today is a Sunday" do
+      time = Time.now - 2.days
+      expect(Project.prev_bucket(time)).to eql "20151211"
+    end
+    it "next bucket is nil if time is current" do
+      expect(Project.next_bucket(Time.now)).to eql nil
+    end
+    it "gets next bucket" do
+      expect(Project.next_bucket(Time.now - 1.day)).to eql "20151215"
+    end
+    it "next bucket is a Monday if today is a Saturday" do
+      time = Time.now - 3.days
+
+      expect(Project.next_bucket(time)).to eql "20151214"
+    end
+    it "next bucket is nil if current day is Saturday" do
+      Timecop.freeze(Time.local(2015, 12, 12, 12, 0, 0))
+      expect(Project.next_bucket(Time.now)).to eql nil
+      Timecop.return
+    end
+
   end
-
-
-  # context "featured" do
-  #   before :all do
-  #     DatabaseCleaner.clean
-  #     # create projects
-  #     25.times do |i|
-  #       user = FactoryGirl.create(:user)
-  #       proj = FactoryGirl.build(:project, created_at: Time.now + (i * 2).hours)
-  #       user.projects << proj
-  #     end
-  #     # create more users
-  #     # each user will vote on 3 random projects.
-  #     50.times do |i|
-  #       user = FactoryGirl.create(:user)
-  #       3.times do
-  #         rand_proj = Project.offset(rand(Project.count)).first
-  #         user.vote(rand_proj)
-  #       end
-  #     end
-  #   end
-  #
-  #   let(:projects_page_1) { Project.featured(1) }
-  #   let(:projects_page_2) { Project.featured(2) }
-  #   let(:projects_page_3) { Project.featured(3) }
-  #
-  #   # One big test because the setup is slow
-  #   it "return paginated results" do
-  #     expect(projects_page_1.length).to eql 10
-  #     expect(projects_page_2.length).to eql 10
-  #     expect(projects_page_3.length).to eql 5
-  #   end
-  #
-  #   it "orders by score" do
-  #     expect(projects_page_1.first.score).to be > projects_page_1.last.score
-  #     expect(projects_page_1.last.id).to_not eql projects_page_2.first.id
-  #     expect(projects_page_1.last.score).to be >= projects_page_2.first.score
-  #   end
-  # end
-
 end
