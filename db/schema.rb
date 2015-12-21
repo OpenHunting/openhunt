4b9afe9cffc8259b2a98c5ad6d9089acfcd3d310
Nathaniel Blackburn <support@nblackburn.uk>
2015-12-21 23:57:22 +0000

    Removed redundant table

--- db/schema.rb
+++ db/schema.rb
@@ -67,13 +67,6 @@ ActiveRecord::Schema.define(version: 20151221232302) do
   add_index "projects", ["bucket"], name: "index_projects_on_bucket", using: :btree
   add_index "projects", ["slug"], name: "index_projects_on_slug", using: :btree
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "role_id_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "screen_name",                       null: false
     t.string   "name"
